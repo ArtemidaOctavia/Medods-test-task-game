e996d07176b2aa847a38d5aea19738885fc28b2e
ArtemidaOctavia <Hippi96@yandex.ru>
2020-11-09 18:05:14 +0300

    upload

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div id="app">
+    <Game></Game>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'app',
+  data () {
+    return {
+
+    }
+  }
+}
+</script>
+
+<style lang="sass">
+
+</style>
